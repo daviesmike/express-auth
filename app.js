9d33550e7c2e0e94bc2a4047436cfece92c152d1
daviesmike <daviesmike527@gmail.com>
2021-11-09 21:21:20 +0100

    added procfile

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs")
+
+const mongoDb = "mongodb+srv://zack:cody@cluster0.9tjq7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+  });
+
+app.post("/", (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  }).save(err => {
+    if (err) { 
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.post("/log-in", (req, res, next) => {
+
+});
+
+
+app.get("/", (req, res) => {
+    res.render("sign-up-form");
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.get("/log-out", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+                return done(null, user)
+            } else {
+                return done(null, false, {message: "Incorrect password"})
+            }
+        })
+      });
+    })
+);
+
+passport.serializeUser(function(user, done) {
+done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+User.findById(id, function(err, user) {
+    done(err, user);
+});
+});
+
+
+
+
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
